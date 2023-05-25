7ee2d13f9c5d0a873de84625641d31bc17cd42d1
Nontapat Thaiprayoon <nontapat@Architects49.com>
2023-05-25 15:07:30 +0700

    update

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
-# import folium
-# import streamlit as st
-
-# from streamlit_folium import st_folium
-
-# # center on Liberty Bell, add marker
-# m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-# folium.Marker(
-#     [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-# ).add_to(m)
-
-# # call to render Folium map in Streamlit
-# st_data = st_folium(m, width=725)
 import pandas as pd
-import numpy as np
 import folium
 from shapely.geometry import Point, Polygon
-import streamlit as st
 from streamlit_folium import st_folium
 
 
