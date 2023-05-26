9ea9d6ddeb49ad5a56617b6eefc242e706872d99
Nontapat Thaiprayoon <nontapat@Architects49.com>
2023-05-26 14:01:16 +0700

    update heatmap

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from folium.plugins import HeatMap, HeatMapWithTime
 df = pd.read_csv('community.csv')
 heatmap_df = pd.read_csv('districtBKK_den_loc.csv')
 # ['เขต', 'ชุมชน (ชุมชน)', 'ประชากร (คน)', 'ครอบครัว (ครอบครัว)''หลังคาเรือน (หลัง)', 'District', 'LAT', 'LONG']
-heatmap_df = heatmap_df[['LAT','LONG','ประชากร (คน)']]
+heatmap_df0 = heatmap_df[['LAT','LONG','ประชากร (คน)']]
+heatmap_df1 = heatmap_df[['LAT','LONG','District']]
 
 # _1 = st.checkbox('ชุมชนแออัด')
 # _2 = st.checkbox('ชุมชนชานเมือง')
@@ -25,10 +26,22 @@ df_5 = df[df['type'] == 'ชุมชนเมือง']
 # df_6 = df[df['type'] == 'ชุมชนอาคารสูง']
 
 map_th0 = folium.Map(location=[13.80174488029037, 100.5863404554943], tiles="Stamen Toner", zoom_start=10)
-HeatMap(heatmap_df, 
+HeatMap(heatmap_df0, 
         min_opacity=0.4,
         blur = 18
                ).add_to(folium.FeatureGroup(name='Heat Map').add_to(map_th0))
+
+for lat, lng, name in zip(heatmap_df1['LAT'].astype(float), heatmap_df1['LONG'].astype(float), 'เขต' + heatmap_df1['District']):
+    folium.CircleMarker(
+        [lat, lng],
+        radius=3,
+        color='#000000',
+        fill=True,
+        popup=folium.Popup(name, max_width="100"),
+        fill_color='#000000',
+        fill_opacity=0.7,
+        parse_html=False
+    ).add_to(map_th0)
 # folium.LayerControl().add_to(map_th0)
 
 map_th = folium.Map(location=[13.80174488029037, 100.5863404554943], tiles="Stamen Toner", zoom_start=10)
